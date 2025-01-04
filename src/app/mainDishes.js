126a0bac8e5d8339fc528b674342cc859b39b3d2
Lukayx <olavarriafernando36@gmail.com>
2025-01-04 05:20:09 -0300

    responsividad y estilos

--- src/app/mainDishes.js
+++ src/app/mainDishes.js
@@ -1,23 +1,56 @@
 "use client";
 import { useState } from "react";
 import styles from "./mainDishes.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUtensils,
+  faDrumstickBite,
+  faWineGlass,
+  faIceCream,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function MainDishes() {
   const [selectedOption, setSelectedOption] = useState(0);
 
   return (
-    <section className={styles.mainDishes} id="main-dishes">
-      <h1>Platos principales</h1>
+    <section className={styles.sectionMainDishes} id="main-dishes">
+      <header className={styles.headerUs}>
+        <h6>Disfrutemos juntos</h6>
+        <h1>Platos principales</h1>
+        <span>◆</span>
+      </header>
       <div className={styles.dishesTitle}>
         <ul>
-          <li onClick={() => setSelectedOption(0)}>Platos Principales</li>
-          <li onClick={() => setSelectedOption(1)}>Carnes</li>
-          <li onClick={() => setSelectedOption(2)}>Vinos</li>
-          <li onClick={() => setSelectedOption(3)}>Postres</li>
+          <li
+            className={selectedOption === 0 ? styles.selected : ""}
+            onClick={() => setSelectedOption(0)}
+          >
+            <FontAwesomeIcon icon={faUtensils} className={styles.icon} />
+            <span>Platos Principales</span>
+          </li>
+          <li
+            className={selectedOption === 1 ? styles.selected : ""}
+            onClick={() => setSelectedOption(1)}
+          >
+            <FontAwesomeIcon icon={faDrumstickBite} className={styles.icon} />
+            <span>Carnes</span>
+          </li>
+          <li
+            className={selectedOption === 2 ? styles.selected : ""}
+            onClick={() => setSelectedOption(2)}
+          >
+            <FontAwesomeIcon icon={faWineGlass} className={styles.icon} />
+            <span>Vinos</span>
+          </li>
+          <li
+            className={selectedOption === 3 ? styles.selected : ""}
+            onClick={() => setSelectedOption(3)}
+          >
+            <FontAwesomeIcon icon={faIceCream} className={styles.icon} />
+            <span>Postres</span>
+          </li>
         </ul>
       </div>
-
-
     </section>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -27,31 +27,25 @@ export default function Home() {
           <a href="#us">Quienes somos</a>
         </header>
         <section className={styles.sectionUs} id="us">
-          <div>
+          <header className={styles.headerUs}>
             <h6>Disfrutemos juntos</h6>
             <h1>Quienes somos</h1>
-            <p>
-              Fundado en 2021, tras los desafíos de la pandemia, nuestro
-              restaurante nació con la pasión de reunir a las personas en torno
-              a la mesa, celebrando lo mejor de la gastronomía chilena.
-              Especializados en carnes y comida típica, ofrecemos una
-              experiencia culinaria única que combina tradición y creatividad.
-            </p>
-            <p>
-              Para quienes buscan opciones vegetarianas, nuestra carta incluye
-              platos cuidadosamente diseñados, además de una selección de vinos
-              de excelencia y tragos de autor que deleitan los sentidos.
-              Cerramos la experiencia con postres que capturan el alma de
-              nuestra cocina. Aquí, cada detalle está pensado para ofrecer una
-              experiencia auténtica y memorable que celebre la riqueza de
-              nuestra tierra y su cultura gastronómica.
-            </p>
-            {/* <p>
-                Somos un restaurante de comida chilena con una amplia variedad
-                de platos y vinos. Nos caracterizamos por nuestra atención y
-                calidad en los productos que ofrecemos.
-              </p> */}
-          </div>
+            <span>◆</span>
+          </header>
+          <span className={styles.card} id={styles.c1}>
+            Fundado en 2021, nuestro restaurante nació con la misión de unir a
+            las personas en torno a la mesa, celebrando lo mejor de la
+            gastronomía chilena.
+          </span>
+          <span className={styles.card} id={styles.c2}>
+            Especializados en carnes y platos típicos, ofrecemos una
+            experiencia única que combina tradición con creatividad.
+          </span>
+          <span className={styles.card} id={styles.c3}>
+            Para quienes buscan opciones vegetarianas, nuestra carta incluye
+            platos cuidadosamente diseñados, vinos de excelencia y postres que
+            capturan el alma de nuestra cocina.
+          </span>
           <Image
             src="/UsHeader.webp"
             className={styles.usImage}
@@ -60,12 +54,12 @@ export default function Home() {
             alt="Quienes somos"
           />
           {/* <Image
-              src="/UsHeader.webp"
-              className={styles.usImagee}
-              width={1920} // Proporción base
-              height={500} // Proporción base
-              alt="Quienes somos"
-            /> */}
+            src="/UsHeader.webp"
+            className={styles.usImagee}
+            width={1920} // Proporción base
+            height={500} // Proporción base
+            alt="Quienes somos"
+          /> */}
         </section>
         <MainDishes />
         <section className={styles.sectionWines} id="wines"></section>
